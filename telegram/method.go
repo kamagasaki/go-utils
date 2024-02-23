a9d2ce15e401f835b125e8b5882ae40269cfb2e6
HeroBuxx <me@herobuxx.me>
2024-02-23 21:40:31 +0700

    utils: telegram: Add function to send file
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- telegram/method.go
+++ telegram/method.go
@@ -4,7 +4,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
+	"mime/multipart"
 	"net/http"
+	"os"
 )
 
 func SendMessage(apiKey string, chatID int64, message string) error {
@@ -29,6 +32,63 @@ func SendMessage(apiKey string, chatID int64, message string) error {
 	}
 	defer resp.Body.Close()
 
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	return nil
+}
+
+func SendDocument(token, chatID, filePath, caption string) error {
+	// Open the file
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Create a new multipart writer
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	// Add the file field
+	part, err := writer.CreateFormFile("document", filePath)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(part, file)
+	if err != nil {
+		return err
+	}
+
+	// Add other form fields
+	_ = writer.WriteField("chat_id", chatID)
+	_ = writer.WriteField("caption", caption)
+	_ = writer.WriteField("parse_mode", "html")
+
+	// Close the multipart writer
+	err = writer.Close()
+	if err != nil {
+		return err
+	}
+
+	// Create the request
+	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendDocument", token)
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	// Send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Check the response status code
 	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
 	}
