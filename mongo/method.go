0a03dc53dd8de8b78ed134e2730d062e359ff864
HeroBuxx <me@herobuxx.me>
2024-02-23 09:33:18 +0700

    utils: Kang ATDB mongo utils
    Co-authored-by: Rolly Maulana Awangga <awangga@gmail.com>
    Co-authored-by: JPratama7 <josepratama080@gmail.com>
    Co-authored-by: Valen Rionald <vrionald@gmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

---
+++ mongo/method.go
@@ -0,0 +1,109 @@
+package mongo
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func MongoConnect(mconn DBInfo) (db *mongo.Database) {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mconn.DBString))
+	if err != nil {
+		fmt.Printf("AIteung Mongo, MongoConnect: %v\n", err)
+	}
+	return client.Database(mconn.DBName)
+}
+
+func InsertOneDoc(db *mongo.Database, collection string, doc interface{}) (insertedID interface{}) {
+	insertResult, err := db.Collection(collection).InsertOne(context.TODO(), doc)
+	if err != nil {
+		fmt.Printf("AIteung Mongo, InsertOneDoc: %v\n", err)
+	}
+	return insertResult.InsertedID
+}
+
+func GetOneDoc[T any](db *mongo.Database, collection string, filter bson.M) (doc T) {
+	err := db.Collection(collection).FindOne(context.TODO(), filter).Decode(&doc)
+	if err != nil {
+		fmt.Printf("GetOneDoc: %v\n", err)
+	}
+	return
+}
+
+func GetOneLatestDoc[T any](db *mongo.Database, collection string, filter bson.M) (doc T, err error) {
+	opts := options.FindOne().SetSort(bson.M{"$natural": -1})
+	err = db.Collection(collection).FindOne(context.TODO(), filter, opts).Decode(&doc)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func GetAllDocByFilter[T any](db *mongo.Database, collection string, filter bson.M) (doc T) {
+	ctx := context.TODO()
+	cur, err := db.Collection(collection).Find(ctx, filter)
+	if err != nil {
+		fmt.Printf("GetAllDoc: %v\n", err)
+	}
+	defer cur.Close(ctx)
+	err = cur.All(ctx, &doc)
+	if err != nil {
+		fmt.Printf("GetAllDoc Cursor Err: %v\n", err)
+	}
+	return
+}
+
+func GetAllDoc[T any](db *mongo.Database, collection string) (doc T) {
+	ctx := context.TODO()
+	cur, err := db.Collection(collection).Find(ctx, bson.M{})
+	if err != nil {
+		fmt.Printf("GetAllDoc: %v\n", err)
+	}
+	defer cur.Close(ctx)
+	err = cur.All(ctx, &doc)
+	if err != nil {
+		fmt.Printf("GetAllDoc Cursor Err: %v\n", err)
+	}
+	return
+}
+
+func GetAllDistinctDoc(db *mongo.Database, filter bson.M, fieldname, collection string) (doc []any) {
+	ctx := context.TODO()
+	doc, err := db.Collection(collection).Distinct(ctx, fieldname, filter)
+	if err != nil {
+		fmt.Printf("GetAllDistinctDoc: %v\n", err)
+	}
+	return
+}
+
+func UpdateDoc(db *mongo.Database, collection string, filter bson.M, updatefield bson.M) (updateresult *mongo.UpdateResult, err error) {
+	updateresult, err = db.Collection(collection).UpdateOne(context.TODO(), filter, updatefield)
+	return
+}
+
+func ReplaceOneDoc(db *mongo.Database, collection string, filter bson.M, doc interface{}) (updatereseult *mongo.UpdateResult) {
+	updatereseult, err := db.Collection(collection).ReplaceOne(context.TODO(), filter, doc)
+	if err != nil {
+		fmt.Printf("ReplaceOneDoc: %v\n", err)
+	}
+	return
+}
+
+func DeleteOneDoc(db *mongo.Database, collection string, filter bson.M) (result *mongo.DeleteResult) {
+	result, err := db.Collection(collection).DeleteOne(context.TODO(), filter)
+	if err != nil {
+		fmt.Printf("DeleteOneDoc: %v\n", err)
+	}
+	return
+}
+
+func DeleteDoc(db *mongo.Database, collection string, filter bson.M) (result *mongo.DeleteResult) {
+	result, err := db.Collection(collection).DeleteMany(context.TODO(), filter)
+	if err != nil {
+		fmt.Printf("DeleteDoc : %v\n", err)
+	}
+	return
+}
